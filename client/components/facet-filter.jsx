fc8f3aae789f6515e7ccfa0d396ddef652b9bf27
Petr Škoda <skodapetr@gmail.com>
2017-05-11 18:44:38 +0200

    Update facet filter

--- client/components/facet-filter.jsx
+++ client/components/facet-filter.jsx
@@ -2,27 +2,88 @@ import React from "react";
 import {PropTypes} from "prop-types";
 import {ListGroup, ListGroupItem} from "reactstrap";
 
-const FacetFilter = ({label, values, active, onChange}) => (
-    <div>
-        <h3>{label}</h3>
-        <ListGroup>
-            {values.map((item) => {
-                const isActive = active.indexOf(item.label) >= 0;
-                return (
-                    <ListGroupItem
-                        key={item.label}
-                        onClick={() => onChange(item)}
-                        action
-                        href="javascript:void(0)"
-                        tag="a">
-                        {item.label} ({item.count})
-                        {isActive && <div>ACTIVE</div> }
-                    </ListGroupItem >
-                );
-            })}
-        </ListGroup>
-    </div>
-);
+class FacetFilter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.generateListItems = this.generateListItems.bind(this);
+        this.state = {
+            "showAll": false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            "showAll": !this.state.showAll
+        });
+    }
+
+    generateListItems() {
+        const props = this.props;
+        let indexEnd;
+        if (this.state.showAll) {
+            indexEnd = props.values.length;
+        } else {
+            indexEnd = Math.min(7, props.values.length);
+        }
+        const items = [];
+        // TODO First show all actives
+        for (let index = 0; index < indexEnd; ++index) {
+            const value = props.values[index];
+            const isActive = props.active.indexOf(value.label) >= 0;
+            const html = (
+                <ListGroupItem
+                    key={value.label}
+                    onClick={() => props.onChange(value)}
+                    action
+                    active={isActive}
+                    href="javascript:void(0)"
+                    tag="a">
+                    {value.label} ({value.count})
+                </ListGroupItem >
+            );
+            items.push(html);
+        }
+        if (props.values.length > 7) {
+            let label;
+            if (this.state.showAll) {
+                label = "Zobrazit pouze populární";
+            } else {
+                label = "Zobrazit další";
+            }
+            const html = (
+                <ListGroupItem
+                    key="ShowNext"
+                    onClick={() => this.toggle()}
+                    action
+                    href="javascript:void(0)"
+                    tag="a">
+                    <strong>{label}</strong>
+                </ListGroupItem >
+            );
+            items.push(html);
+        }
+
+        return items;
+    }
+
+    render() {
+        const props = this.props;
+        const items = this.generateListItems();
+
+        return (
+            <div style={{"marginTop": "1em"}}>
+                <h3 style={{
+                    "padding": "0.2em 0em 0.2em 1em"
+                }}>{props.label}</h3>
+                <ListGroup>
+                    {items}
+                </ListGroup>
+            </div>
+        )
+    }
+}
 
 FacetFilter.propTypes = {
     "label": PropTypes.string.isRequired,
