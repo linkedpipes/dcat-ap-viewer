15a8b529aaf4ca9f60089b2a3ba622f11dee3af0
Petr Škoda <skodapetr@gmail.com>
2017-05-11 18:59:09 +0200

    Update page header

--- client/application/header.jsx
+++ client/application/header.jsx
@@ -1,13 +1,101 @@
 import React from "react";
-import {Container, Row, Col} from "reactstrap";
+import {
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Container,
+    Collapse,
+    Nav,
+    NavItem,
+    NavLink,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from "reactstrap";
 
-const Header = () => (
-    <Container fluid={true}>
-        <Row>
-            <Col style={{backgroundColor: "lightblue", height: "2em"}}>
-            </Col>
-        </Row>
-    </Container>
-);
+class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.toggleDropDown = this.toggleDropDown.bind(this);
+        this.state = {
+            "isOpen": false,
+            "dropdownOpen": false
+        };
+    }
+
+    toggleNavbar() {
+        this.setState({
+            "isOpen": !this.state.isOpen
+        });
+    }
+
+    toggleDropDown() {
+        this.setState({
+            "dropdownOpen": !this.state.dropdownOpen
+        });
+    }
+
+    render() {
+        return (
+            <Navbar toggleable="md">
+                <Container>
+                    <NavbarBrand href="#">
+                        <img width="174" height="30"
+                             className="d-inline-block align-top"
+                             src="assets/images/opendata-logo.png"/>
+                    </NavbarBrand>
+                    <NavbarToggler right onClick={this.toggleNavbar}/>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/">
+                                    Datové sady
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/">
+                                    Poskytovatelé
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/">
+                                    Aplikace
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <Dropdown isOpen={this.state.dropdownOpen}
+                                          toggle={this.toggleDropDown}>
+                                    <DropdownToggle caret nav>
+                                        Další
+                                    </DropdownToggle>
+                                    <DropdownMenu>
+                                        <DropdownItem>
+                                            <NavLink href="/">
+                                                Pro zájemce o otevírání dat
+                                            </NavLink>
+                                        </DropdownItem>
+                                        <DropdownItem>
+                                            <NavLink href="/">
+                                                Pro uživatele a programátory
+                                            </NavLink>
+                                        </DropdownItem>
+                                        <DropdownItem>
+                                            <NavLink href="/">
+                                                Pro poskytovatele dat
+                                            </NavLink>
+                                        </DropdownItem>
+                                    </DropdownMenu>
+                                </Dropdown>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        )
+    }
+}
 
 export default Header;
