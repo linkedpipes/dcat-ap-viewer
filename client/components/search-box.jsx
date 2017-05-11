01b27e650e903122cd9bf433b0dde2ee3669f07c
Petr Škoda <skodapetr@gmail.com>
2017-05-11 18:48:13 +0200

    Remove margin from search box

--- client/components/search-box.jsx
+++ client/components/search-box.jsx
@@ -1,35 +1,26 @@
 import React from "react";
-import {
-    InputGroup,
-    InputGroupAddon,
-    InputGroupButton,
-    Button,
-    Input
-} from "reactstrap";
+import {InputGroup, InputGroupButton, Button, Input} from "reactstrap";
 
 const SearchBox = ({value, onChange, onSearch}) => (
-    <div style={{margin: "1em 1em 1em 1em"}}>
-        <InputGroup>
-            <InputGroupAddon>Query</InputGroupAddon>
-            <Input
-                value={value}
-                onChange={(event) => {
-                    onChange(event.target.value);
-                }}
-                onKeyPress={(event) => {
-                    if (event.key === "Enter") {
-                        onSearch(value);
-                    }
-                }}
-                placeholder="Search datasets..."
-                type="text"/>
-            <InputGroupButton>
-                <Button onClick={() => onSearch(value)} color="secondary">
-                    Search
-                </Button>
-            </InputGroupButton>
-        </InputGroup>
-    </div>
+    <InputGroup>
+        <Input
+            value={value}
+            onChange={(event) => {
+                onChange(event.target.value);
+            }}
+            onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                    onSearch(value);
+                }
+            }}
+            placeholder="Vyhledat datasety ..."
+            type="text"/>
+        <InputGroupButton>
+            <Button onClick={() => onSearch(value)} color="secondary">
+                Hledat
+            </Button>
+        </InputGroupButton>
+    </InputGroup>
 );
 
 export default SearchBox;
