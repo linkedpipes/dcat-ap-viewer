9e222e6a7ae1b2a28295801557d13457273fad76
Petr Škoda <skodapetr@gmail.com>
2017-05-11 18:44:46 +0200

    Update paginator

--- client/components/paginator.jsx
+++ client/components/paginator.jsx
@@ -3,36 +3,49 @@ import {PropTypes} from "prop-types";
 import {Pagination, PaginationItem, PaginationLink} from "reactstrap";
 
 // TODO Add active state https://reactstrap.github.io/components/pagination/
-const Paginator = ({start, end, value, onChange}) => (
-    <Pagination>
-        {value != start &&
-        <PaginationItem>
-            <PaginationLink previous href="#" onClick={() => onChange(start)}/>
-        </PaginationItem>
-        }
-        {(() => {
-            const pageReferences = [];
-            const indexStart = Math.max(start, value - 2);
-            const indexEnd = Math.min(value + 3, end);
-            for (let index = indexStart; index != indexEnd; ++index) {
-                pageReferences.push((
-                    <PaginationItem key={index}>
-                        <PaginationLink href="javascript:void(0)"
-                                        onClick={() => onChange(index)}>
-                            {index + 1}
-                        </PaginationLink>
-                    </PaginationItem>
-                ));
-            }
-            return pageReferences;
-        })()}
-        {end != value &&
-        <PaginationItem>
-            <PaginationLink next href="#" onClick={() => onChange(end - 1)}/>
-        </PaginationItem>
-        }
-    </Pagination>
-);
+
+// TODO Add dots 1 ... 9 10 11 ... 199
+
+const Paginator = ({start, end, value, onChange}) => {
+    console.log(start, end);
+    if (start === end) {
+        return null;
+    }
+    return (
+        <Pagination>
+            <PaginationItem active={start == value}>
+                <PaginationLink
+                    href="javascript:void(0)"
+                    onClick={() => onChange(start)}>
+                    {start + 1}
+                </PaginationLink>
+            </PaginationItem>
+            {(() => {
+                const pageReferences = [];
+                const indexStart = Math.max(start + 1, value - 2);
+                const indexEnd = Math.min(value + 3, end - 1);
+                for (let index = indexStart; index < indexEnd; ++index) {
+                    pageReferences.push((
+                        <PaginationItem key={index} active={index == value}>
+                            <PaginationLink href="javascript:void(0)"
+                                            onClick={() => onChange(index)}>
+                                {index + 1}
+                            </PaginationLink>
+                        </PaginationItem>
+                    ));
+                }
+                return pageReferences;
+            })()}
+            <PaginationItem active={end == value}>
+                <PaginationLink
+                    href="javascript:void(0)"
+                    onClick={() => onChange(end)}>
+                    {end + 1}
+                </PaginationLink>
+            </PaginationItem>
+        </Pagination>
+    )
+}
 
 Paginator.propTypes = {
     "start": PropTypes.number.isRequired,
