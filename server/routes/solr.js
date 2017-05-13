e32fae341f5b086ccfafbddac148759a22cd0f41
Petr Škoda <skodapetr@gmail.com>
2017-05-13 15:07:07 +0200

    Add route with information about data

--- server/routes/solr.js
+++ server/routes/solr.js
@@ -5,7 +5,29 @@
 
     const router = express.Router();
 
-    // Forward to Solr.
+    router.get("/info", function (req, res) {
+        const url = configuration.solr.url + "/query?q=*:*";
+        const options = {
+            "url": url
+        };
+        request(options, function (error, response, body) {
+            if (error === null && response && response.statusCode == 200) {
+                const content = JSON.parse(body);
+                res.json({
+                    "data": {
+                        "numberOfDatasets": content.response.numFound
+                    }
+                });
+            } else {
+                res.status(500);
+                res.json({
+                    "error": error
+                });
+            }
+        });
+    });
+
+    // Forward to Solr query.
     router.get("/query", function (req, res) {
         const url = configuration.solr.url + req.url;
         const options = {
