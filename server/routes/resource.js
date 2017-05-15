54eebf1ec60310fa0dc4d4a1bc8d663870452a45
Petr Škoda <skodapetr@gmail.com>
2017-05-15 08:30:32 +0200

    Extend SPARQL queries
    Add additional information about selected resources.

--- server/routes/resource.js
+++ server/routes/resource.js
@@ -5,21 +5,13 @@
 
     const router = express.Router();
 
-    // https://nkod.opendata.cz/sparql/?
-    //  default-graph-uri=&
-    //  query=describe+%3Chttps%3A%2F%2Fnkod.opendata.cz%2Fzdroj%2Fdatov%C3%A1-sada%2F100130707%3E& -> use ?s ?p ?o as describe includes also incomming links
-    //  format=application%2Fx-json%2Bld&
-    //  timeout=0&
-    //  debug=on
-
     router.get("/dataset", function (req, res) {
         // TODO Extract Virtuoso query to function
         const datasetIri = req.query.iri;
         const url = configuration.sparql.url + "/?" +
             "format=application%2Fx-json%2Bld&" +
             "timeout=0&" +
-            "query=" + encodeURI("CONSTRUCT WHERE { <" + datasetIri + "> ?p ?o }");
-
+            "query=" + encodeURIComponent(getDatasetSparqlQuery(datasetIri));
         const options = {
             "url": url
         };
@@ -30,12 +22,12 @@
 
     router.get("/distribution", function (req, res) {
         // TODO Same as for /dataset
-
         const datasetIri = req.query.iri;
         const url = configuration.sparql.url + "/?" +
             "format=application%2Fx-json%2Bld&" +
             "timeout=0&" +
-            "query=" + encodeURI("CONSTRUCT WHERE { <" + datasetIri + "> ?p ?o }");
+            "query=" + encodeURIComponent(
+                getDistributionSparqlQuery(datasetIri));
 
         const options = {
             "url": url
@@ -47,3 +39,75 @@
 
     module.exports = router;
 })();
+
+function getDatasetSparqlQuery(iri) {
+    return "" +
+        "PREFIX dcat: <http://www.w3.org/ns/dcat#> " +
+        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+        "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " +
+        "PREFIX dcterms: <http://purl.org/dc/terms/> " +
+        "PREFIX foaf: <http://xmlns.com/foaf/0.1/> " +
+        "PREFIX schema: <http://schema.org/> " +
+        "PREFIX vcard: <http://www.w3.org/2006/vcard/ns#> " +
+        "PREFIX void: <http://rdfs.org/ns/void#> " +
+        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+        "PREFIX adms: <http://www.w3.org/ns/adms#> " +
+        "PREFIX spdx: <http://spdx.org/rdf/terms#> " +
+        "PREFIX org: <http://www.w3.org/ns/org#> " +
+        "" +
+        "CONSTRUCT { " +
+        "?Dataset ?p ?o .  " +
+        "  " +
+        "?cp vcard:fn ?cpfn ; " +
+        "   vcard:hasEmail ?cpemail .  " +
+        " " +
+        "?publisher a foaf:Agent; " +
+        "   foaf:name ?name .  " +
+        "?temporal schema:startDate ?temporalStart ; " +
+        "   schema:endDate ?temporalEnd . " +
+        "} WHERE {  " +
+        "?Dataset ?p ?o . " +
+        "" +
+        "OPTIONAL { ?Dataset dcterms:modified ?modified . } " +
+        "OPTIONAL { ?Dataset dcterms:accrualPeriodicity ?accrualPeriodicity . } " +
+        "OPTIONAL { ?Dataset dcterms:issued ?issued . } " +
+        "OPTIONAL { ?Dataset dcterms:language ?language . } " +
+        "OPTIONAL { ?Dataset dcterms:identifier ?identifier . } " +
+        "OPTIONAL { ?Dataset dcterms:type ?type . } " +
+        "OPTIONAL { ?Dataset foaf:page ?page . } " +
+        "OPTIONAL { ?Dataset dcat:theme ?theme . } " +
+        "OPTIONAL { ?Dataset dcat:landingPage ?landingPage . } " +
+        "OPTIONAL { ?Dataset dcat:keyword ?keyword . } " +
+        "OPTIONAL {  " +
+        "   ?Dataset dcat:contactPoint ?cp .  " +
+        "   ?cp vcard:fn ?cpfn ; " +
+        "       vcard:hasEmail ?cpemail .  " +
+        "} " +
+        "OPTIONAL {  " +
+        "   ?Dataset dcterms:publisher ?publisher .  " +
+        "   ?publisher a foaf:Agent; " +
+        "       foaf:name ?name .  " +
+        "} " +
+        "OPTIONAL {" +
+        "    ?Dataset dcterms:temporal ?temporal . " +
+        "    ?temporal schema:startDate ?temporalStart ; " +
+        "       schema:endDate ?temporalEnd . " +
+        "} " +
+        "VALUES (?Dataset) { (<" + iri + ">) } " +
+        "}";
+}
+
+function getDistributionSparqlQuery(iri) {
+    return "" +
+        "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " +
+        "PREFIX dcterms: <http://purl.org/dc/terms/> " +
+        "" +
+        "CONSTRUCT WHERE { " +
+        "   <" + iri + "> ?p ?o . " +
+        "OPTIONAL { " +
+        "   <" + iri + "> dcterms:format ?format . " +
+        "   ?format skos:prefLabel ?formatLabel. " +
+        "}" +
+        "}";
+}
