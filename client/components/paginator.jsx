9c3abd71d368c0bdde177d9087641c05b04d42d3
Petr Škoda <skodapetr@gmail.com>
2017-07-18 09:17:26 +0200

    Update paginator
    #17

--- client/components/paginator.jsx
+++ client/components/paginator.jsx
@@ -2,47 +2,82 @@ import React from "react";
 import {PropTypes} from "prop-types";
 import {Pagination, PaginationItem, PaginationLink} from "reactstrap";
 
-// TODO Add active state https://reactstrap.github.io/components/pagination/
-
-// TODO Add dots 1 ... 9 10 11 ... 199
-
 const Paginator = ({start, end, value, onChange}) => {
     if (start === end) {
         return null;
     }
+    const pages = createPageList(start, end, value);
+    const pageItems = createPageItemsList(pages, value, onChange);
     return (
         <Pagination>
-            <PaginationItem active={start == value}>
-                <PaginationLink
-                    href="javascript:void(0)"
-                    onClick={() => onChange(start)}>
-                    {start + 1}
-                </PaginationLink>
-            </PaginationItem>
-            {(() => {
-                const pageReferences = [];
-                const indexStart = Math.max(start + 1, value - 2);
-                const indexEnd = Math.min(value + 3, end - 1);
-                for (let index = indexStart; index < indexEnd; ++index) {
-                    pageReferences.push((
-                        <PaginationItem key={index} active={index == value}>
-                            <PaginationLink href="javascript:void(0)"
-                                            onClick={() => onChange(index)}>
-                                {index + 1}
-                            </PaginationLink>
-                        </PaginationItem>
-                    ));
-                }
-                return pageReferences;
-            })()}
-            <PaginationItem active={end == value}>
-                <PaginationLink
-                    href="javascript:void(0)"
-                    onClick={() => onChange(end)}>
-                    {end + 1}
-                </PaginationLink>
-            </PaginationItem>
+            {pageItems}
         </Pagination>
+    )
+};
+
+function createPageList(start, end, value) {
+    // FORMAT: 1, 2, ... V - 2, V - 1, V, V + 1, V + 2, ... E - 1, E
+    if (end - start < 9) {
+        return createArrayWithRangeOfValues(start, end);
+    }
+    const output = [0, 1];
+    const rangeValueStart = Math.max(value - 2, start);
+    const rangeValueEnd = Math.min(value + 2, end);
+    for (let index = rangeValueStart; index <= rangeValueEnd; ++index) {
+        appendIfGreaterThenLast(output, index);
+    }
+    appendIfGreaterThenLast(output, end - 2);
+    appendIfGreaterThenLast(output, end - 1);
+    return output;
+}
+
+function createArrayWithRangeOfValues(start, end) {
+    const output = [];
+    for (let index = start; index < end; ++index) {
+        output.push(index);
+    }
+    return output;
+}
+
+function appendIfGreaterThenLast(array, value) {
+    if (array[array.length - 1] < value) {
+        array.push(value);
+    }
+}
+
+function createPageItemsList(pages, active, onChange) {
+    const output = [];
+    output.push(createPageItem(pages[0], pages[0] == active, onChange));
+    for (let index = 1; index < pages.length; ++index) {
+        const currentPage = pages[index];
+        const expectedCurrentPage = pages[index - 1] + 1;
+        const isActive = currentPage == active;
+        if (currentPage !== expectedCurrentPage) {
+            output.push(createItemForIndexGap(expectedCurrentPage));
+        }
+        output.push(createPageItem(currentPage, isActive, onChange));
+    }
+    return output;
+}
+
+function createPageItem(index, isActive, onChange) {
+    return (
+        <PaginationItem active={isActive} key={index}>
+            <PaginationLink href="javascript:void(0)"
+                            onClick={() => onChange(index)}>
+                {index + 1}
+            </PaginationLink>
+        </PaginationItem>
+    )
+}
+
+function createItemForIndexGap(index) {
+    return (
+        <PaginationItem key={"stub_" + index}>
+            <div style={{"padding": "0.5rem 0.75rem"}}>
+                ...
+            </div>
+        </PaginationItem>
     )
 }
 
