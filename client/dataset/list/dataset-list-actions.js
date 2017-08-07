a5997749d7d67f8b15e06d3af5cbe6cc375f7679
Petr Škoda <skodapetr@gmail.com>
2017-08-07 15:19:08 +0200

    Update used Solr query

--- client/dataset/list/dataset-list-actions.js
+++ client/dataset/list/dataset-list-actions.js
@@ -42,8 +42,29 @@ function constructSearchQueryUrl(query) {
 }
 
 function escapeSolrQuery(query) {
+    // Convert to lowe case.
+    query = query.toLocaleLowerCase();
+
+    // Escape control characters
     const pattern = /([\!\*\+\-\=\<\>\&\|\(\)\[\]\{\}\^\~\?\:\\/"])/g;
-    return query.replace(pattern, "\\$1");
+    query = query.replace(pattern, "\\$1");
+
+    // Escape control words (and, or, not).
+    query = query.replace("and", "\\and");
+    query = query.replace("or", "\\or");
+    query = query.replace("not", "\\not");
+
+    // Update query.
+    const tokens = query.trim().split(" ").filter(value=>value.length > 0);
+    if (tokens.length === 0){
+        return "";
+    }
+    let solrQuery = "*" + tokens[0] + "*";
+    for (let index = 1; index < tokens.length; ++index ) {
+        solrQuery += " AND *" + tokens[index] + "*";
+    }
+
+    return solrQuery;
 }
 
 export function fetchDataRequest(query) {
