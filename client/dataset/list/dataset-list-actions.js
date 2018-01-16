ba2f07b0cc81dad75a690b627ce1d7020fb26eda
Petr Škoda <skodapetr@gmail.com>
2018-01-16 15:28:34 +0100

    Fix solr query with special characters
    Move solr query generation into a solr-api file, remove tokens with
    only one special character from the query.

--- client/dataset/list/dataset-list-actions.js
+++ client/dataset/list/dataset-list-actions.js
@@ -3,6 +3,7 @@ import {
     addLoaderStatusOn,
     addLoaderStatusOff
 } from "../../components/loading-indicator";
+import {constructSearchQueryUrl} from "./../solr-api";
 
 export const FETCH_LIST_PAGE_REQUEST = "FETCH_LIST_PAGE_REQUEST";
 export const FETCH_LIST_PAGE_SUCCESS = "FETCH_LIST_PAGE_SUCCESS";
@@ -26,89 +27,6 @@ function fetchDataRequest() {
     return addLoaderStatusOn({
         "type": FETCH_LIST_PAGE_REQUEST
     });
-}
-
-function constructSearchQueryUrl(query) {
-    let url = "api/v1/solr/query?" +
-        "facet.field=keyword&" +
-        "facet.field=formatName&" +
-        "facet.field=publisherName&" +
-        "facet=true&" +
-        "facet.mincount=1&";
-
-    const start = query.page * query.pageSize;
-    url += "start=" + start + "&";
-
-    if (query.search === undefined || query.search === "") {
-        url += "q=*:*";
-    } else {
-        url += "q=" + encodeURI(escapeSolrQuery(query.search))
-    }
-
-    query.keyword.forEach((item) => {
-        url += "&fq=keyword:\"" + encodeURI(item) + "\""
-    });
-
-    query.publisher.forEach((item) => {
-        url += "&fq=publisherName:\"" + encodeURI(item) + "\""
-    });
-
-    query.format.forEach((item) => {
-        url += "&fq=formatName:\"" + encodeURI(item) + "\""
-    });
-
-    if (query.sort === undefined) {
-        url += "&sort=modified desc";
-    } else {
-        url += "&sort=" + query.sort;
-    }
-
-    url += "&rows=" + query.pageSize;
-
-    if (query.temporalStart === "") {
-        if (query.temporalEnd === "") {
-            // No temporal limits.
-        } else {
-            // Only temporal end is set.
-            url += "&fq=temporal-start:%5B+*+TO+" + query.temporalEnd + "T00%5C:00%5C:00Z+%5D";
-        }
-    } else {
-        if (query.temporalEnd === "") {
-            // Only temporal start is set.
-            url += "&fq=temporal-end:%5B+" + query.temporalStart+ "T00%5C:00%5C:00Z+TO+*+%5D";
-        } else {
-            // Both temporal values are set.
-            url += "&fq=temporal-start:%5B+*+TO+" + query.temporalStart + "T00%5C:00%5C:00Z+%5D";
-            url += "&fq=temporal-end:%5B+" + query.temporalEnd + "T00%5C:00%5C:00Z+TO+*+%5D";
-        }
-    }
-    return url;
-}
-
-function escapeSolrQuery(query) {
-    // Convert to lowe case.
-    query = query.toLocaleLowerCase();
-
-    // Escape control characters
-    const pattern = /([\!\*\+\-\=\<\>\&\|\(\)\[\]\{\}\^\~\?\:\\/"])/g;
-    query = query.replace(pattern, "\\$1");
-
-    // Escape control words (and, or, not).
-    query = query.replace("and", "\\and");
-    query = query.replace("or", "\\or");
-    query = query.replace("not", "\\not");
-
-    // Update query.
-    const tokens = query.trim().split(" ").filter(value => value.length > 0);
-    if (tokens.length === 0) {
-        return "";
-    }
-    let solrQuery = "*" + tokens[0] + "*";
-    for (let index = 1; index < tokens.length; ++index) {
-        solrQuery += " AND *" + tokens[index] + "*";
-    }
-
-    return solrQuery;
 }
 
 function fetchDataSuccess(json) {
--- client/dataset/solr-api.js
+++ client/dataset/solr-api.js
@@ -18,11 +18,16 @@ function escapeSolrQuery(query) {
     query = query.replace("or", "\\or");
     query = query.replace("not", "\\not");
 
-    // Update query.
-    const tokens = query.trim().split(" ").filter(value => value.length > 0);
+    // Tokenize string.
+    const tokens = query.trim().split(" ")
+    .filter(filterEmpty)
+    .filter(filterSpecial);
+
     if (tokens.length === 0) {
         return "";
     }
+    console.log("tokens", tokens);
+
     let solrQuery = "*" + tokens[0] + "*";
     for (let index = 1; index < tokens.length; ++index) {
         solrQuery += " AND *" + tokens[index] + "*";
@@ -31,14 +36,72 @@ function escapeSolrQuery(query) {
     return solrQuery;
 }
 
+function filterEmpty(value) {
+    return value.length > 0;
+}
 
+/**
+ * Return true if value is escaped special character.
+ */
+function filterSpecial(value) {
+    return value[0] !== "\\" || value.length !== 2;
+}
 
+export function constructSearchQueryUrl(query) {
+    let url = "api/v1/solr/query?" +
+        "facet.field=keyword&" +
+        "facet.field=formatName&" +
+        "facet.field=publisherName&" +
+        "facet=true&" +
+        "facet.mincount=1&";
 
+    const start = query.page * query.pageSize;
+    url += "start=" + start + "&";
 
+    console.log("QUERY:", query.search, "->", escapeSolrQuery(query.search));
 
+    if (query.search === undefined || query.search === "") {
+        url += "q=*:*";
+    } else {
+        url += "q=" + encodeURI(escapeSolrQuery(query.search))
+    }
 
+    query.keyword.forEach((item) => {
+        url += "&fq=keyword:\"" + encodeURI(item) + "\""
+    });
 
+    query.publisher.forEach((item) => {
+        url += "&fq=publisherName:\"" + encodeURI(item) + "\""
+    });
 
+    query.format.forEach((item) => {
+        url += "&fq=formatName:\"" + encodeURI(item) + "\""
+    });
 
+    if (query.sort === undefined) {
+        url += "&sort=modified desc";
+    } else {
+        url += "&sort=" + query.sort;
+    }
 
+    url += "&rows=" + query.pageSize;
 
+    if (query.temporalStart === "") {
+        if (query.temporalEnd === "") {
+            // No temporal limits.
+        } else {
+            // Only temporal end is set.
+            url += "&fq=temporal-start:%5B+*+TO+" + query.temporalEnd + "T00%5C:00%5C:00Z+%5D";
+        }
+    } else {
+        if (query.temporalEnd === "") {
+            // Only temporal start is set.
+            url += "&fq=temporal-end:%5B+" + query.temporalStart + "T00%5C:00%5C:00Z+TO+*+%5D";
+        } else {
+            // Both temporal values are set.
+            url += "&fq=temporal-start:%5B+*+TO+" + query.temporalStart + "T00%5C:00%5C:00Z+%5D";
+            url += "&fq=temporal-end:%5B+" + query.temporalEnd + "T00%5C:00%5C:00Z+TO+*+%5D";
+        }
+    }
+    return url;
+}
