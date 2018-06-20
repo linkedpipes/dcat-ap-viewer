25aea9df9d831eed83713e72b8dc626f809f1828
Petr Škoda <skodapetr@gmail.com>
2018-06-20 11:51:42 +0200

    Remove customizable footer

--- client/app/footer.jsx
+++ client/app/footer.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 
-let UserFooterModule = undefined;
-
-(function loadUserProvidedFooter() {
-    if (FOOTER_FILE_PATH !== undefined) {
-        UserFooterModule = require("./../../" + FOOTER_FILE_PATH);
-    }
-})();
-
 const Footer = () => {
-    if (UserFooterModule === undefined) {
-        return null;
-    } else {
-        return (
-            <UserFooterModule.Footer/>
-        )
-    }
-
+    return (
+        <footer>
+            
+        </footer>
+    )
 };
 
 export default Footer;
