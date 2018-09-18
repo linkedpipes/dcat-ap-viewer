87237244f395119d6f3f0f9f0629ff744f92c25b
Petr Škoda <skodapetr@gmail.com>
2018-09-18 09:48:05 +0200

    Remove unused code

--- client/app-ui/paginator.jsx
+++ client/app-ui/paginator.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import {PropTypes} from "prop-types";
 import {
-    Pagination,
-    PaginationItem,
-    Button,
-    ButtonDropdown,
     DropdownToggle,
     DropdownMenu,
     DropdownItem,
