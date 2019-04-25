f55ee724b7aedfb8b6718edc60cf2908deaf5bbc
Petr Škoda <skodapetr@gmail.com>
2019-04-25 10:40:30 +0200

    Add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "verbose": true,
+  "moduleNameMapper": {
+    "^@\/(.*)$": "<rootDir>/client/$1",
+    // Enable import of style files with preserving className lookups.
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  },
+  "modulePaths": [
+    "<rootDir>/client/"
+  ],
+  "transform": {
+    ".*": "babel-jest"
+  },
+  "roots" : [
+    "<rootDir>/client/"
+  ]
+};
+
