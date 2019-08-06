0db1c6c151c85b86c2a06b0a298e19263c63ab38
Petr Škoda <skodapetr@gmail.com>
2019-08-06 11:02:36 +0200

    Fix icon color

--- client/dataset/detail/distribution/terms-of-use/personal-data.jsx
+++ client/dataset/detail/distribution/terms-of-use/personal-data.jsx
@@ -5,17 +5,22 @@ import {PU_VALUES_MAPPING} from "@/app-services/vocabulary";
 export default function personalData(distribution, openModal) {
     let label;
     let title;
+    let iconClass = "text-success";
     const mapped = PU_VALUES_MAPPING[distribution.personalData];
     if (mapped === "no") {
+        iconClass = " text-success";
         label = getString("license_personal_no");
         title = getString("license_personal_no_comment");
     } else if (mapped === "contains") {
+        iconClass = " text-success";
         label = getString("license_personal_yes");
         title = getString("license_personal_yes_comment");
     } else if (mapped === "unspecified") {
+        iconClass = " text-danger";
         label = getString("license_personal_unspecified");
         title = getString("license_personal_unspecified_comment");
     } else if (mapped === "missing") {
+        iconClass = " text-danger";
         label = getString("license_personal_unspecified");
         title = getString("license_personal_missing_comment");
     } else {
@@ -28,7 +33,7 @@ export default function personalData(distribution, openModal) {
         <li className="list-group-item px-2">
             <div>
                 {label}
-                <i className="material-icons text-success float-right"
+                <i className={"material-icons float-right" + iconClass}
                    title={title}
                    onClick={() => openModal(title)}>
                     person
