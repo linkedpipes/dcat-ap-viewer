cf6d58b96f8b688dcc9e553f64b93a7e31986245
Petr Škoda <skodapetr@gmail.com>
2019-09-30 11:29:13 +0200

    Made publisher list component optional

--- client/publisher/list/index.js
+++ client/publisher/list/index.js
@@ -3,24 +3,27 @@ import {PublisherListContainer} from "./publisher-list-container";
 import {ORGANISATION_LIST_URL} from "@/app/navigation";
 import reducer from "@/publisher/publisher-reducer";
 
-register({
-  "name": reducer.name,
-  "url": [ORGANISATION_LIST_URL],
-  "component": PublisherListContainer,
-  "strings": {
-    "cs": {
-      "publishers_found": "poskytovatelů nalezeno"
-      , "one_dataset": "1 datová sada"
-      , "two_three_datasets": " datové sady"
-      , "many_datasets": " datových sad"
-      , "exceptional_publisher": "Vzorný poskytovatel",
+if (SHOW_PUBLISHER_TAB) {
+  register({
+    "name": reducer.name,
+    "url": [ORGANISATION_LIST_URL],
+    "component": PublisherListContainer,
+    "strings": {
+      "cs": {
+        "publishers_found": "poskytovatelů nalezeno"
+        , "one_dataset": "1 datová sada"
+        , "two_three_datasets": " datové sady"
+        , "many_datasets": " datových sad"
+        , "exceptional_publisher": "Vzorný poskytovatel",
+      },
+      "en": {
+        "publishers_found": "publishers found"
+        , "one_dataset": "1 dataset"
+        , "two_three_datasets": " datasets"
+        , "many_datasets": " datasets"
+        , "exceptional_publisher": "Exemplary provider",
+      },
     },
-    "en": {
-      "publishers_found": "publishers found"
-      , "one_dataset": "1 dataset"
-      , "two_three_datasets": " datasets"
-      , "many_datasets": " datasets"
-      , "exceptional_publisher": "Exemplary provider",
-    },
-  },
-});
+  });
+}
+
