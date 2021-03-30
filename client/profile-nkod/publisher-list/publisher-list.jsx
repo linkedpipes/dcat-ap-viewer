b853026e58a88ea4c15448badab5d4348736030b
Jakub Klímek <jakub@jakubklimek.com>
2021-03-30 08:36:16 +0200

    Add flex-fill as necessary

--- client/profile-nkod/publisher-list/publisher-list.jsx
+++ client/profile-nkod/publisher-list/publisher-list.jsx
@@ -68,7 +68,7 @@ function PublisherListItem(props) {
   return (
     <div className="col-12 col-sm-12 col-md-4 col-lg-3 
                     col-xl-3 col-xxl-2 mb-3 d-flex">
-      <Card className="p-2">
+      <Card className="p-2 flex-fill">
         <CardBody className="px-2">
           <CardTitle tag="h5">
             <Link to={getPublisherSearchLink(props.language, props.publisher)}>
