31286a3a023b9e473c288ae2c7cd45af8f987e04
Petr Škoda <skodapetr@gmail.com>
2021-10-02 21:25:13 +0200

    Update use-cases

--- client/dataset-similarity-evaluation/use-cases.ts
+++ client/dataset-similarity-evaluation/use-cases.ts
@@ -1,10 +1,21 @@
 export default [
   {
-    "key": "test-000",
-    "title": "Testovaci use-case",
-  },
-  {
-    "key": "test-001",
-    "title": "Další use-case",
-  },
+    "key": "use-case-001",
+    "title": "Ochrana duševního vlastnictví",
+  }, {
+    "key": "use-case-002",
+    "title": "Zdravotnická zařízení",
+  }, {
+    "key": "use-case-003",
+    "title": "Stavba v Brně",
+  }, {
+    "key": "use-case-004",
+    "title": "Povodně",
+  }, {
+    "key": "use-case-005",
+    "title": "Stav klimatu",
+  }, {
+    "key": "use-case-006",
+    "title": "Pokutované osoby",
+  }
 ];
